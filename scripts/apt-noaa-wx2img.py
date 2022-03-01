bce79616e60c8eb2caa8c500b7b8b70ac5966e29
Angel <arf20@raspberrypi>
2022-03-01 09:50:19 +0100

    wx2img script finally works

--- scripts/apt-noaa-wx2img.py
+++ scripts/apt-noaa-wx2img.py
@@ -1,51 +1,90 @@
 import altiwx
 import subprocess
 import os
-from datetime import datetime
+import datetime
+import time
+import sys
 
 
-# enhencements that should be calculated
-enh = ["HVCT", "MSA", "HVC", "HVC", "HVC-precip",
+try:
+    # enhencements that should be calculated
+    enh = ["HVCT", "MSA", "HVC", "HVC", "HVC-precip",
        "HVCT", "HVCT-precip", "MCIR", "MCIR-precip"]
 
-# define tmporary directory to use while calculating - mind the slash at the end
-tempdir = "/var/ramfs/"
+    # define tmporary directory to use while calculating - mind the slash at the end
+    tempdir = "/tmp/"
 
-# creating a dateobject from the filename
-datetime_object = datetime.strptime(os.path.split(
-    os.path.abspath(altiwx.input_file))[1], '%Y%m%dT%H%M%SZ')
+    # creating a dateobject from the filename
+    #datetime_object = datetime.strptime(os.path.splitext(
+    #    os.path.split(
+    #        os.path.abspath(altiwx.input_file)
+    #    )[1]
+    #)[0], '%Y%m%dT%H%M%SZ')
 
-output_file = os.path.split(os.path.abspath(altiwx.input_file))[1]
+    abspath = os.path.abspath(altiwx.input_file)
+    #altiwx.debug("abspath = " + abspath)
 
-command = "cp '"+altiwx.input_file+".wav' " + tempdir
+    split1 = os.path.split(abspath)[1]
+    #altiwx.debug("split[1] = " + split1)
 
-altiwx.info("Processing NOAA APT data...(extended version)")
+    splitext0 = os.path.splitext(split1)[0]
+    #altiwx.debug("splitext[0] = " + splitext0)
 
-altiwx.debug(command)
-subprocess.Popen([command], shell=1).wait()
+    try:
+        datetime_object = datetime.datetime.strptime(splitext0, '%Y%m%dT%H%M%SZ')
+    except TypeError:
+        datetime_object = datetime.datetime(*(time.strptime(splitext0, '%Y%m%dT%H%M%SZ')[0:6]))
+    #altiwx.debug("strptime")
 
+    output_file = os.path.splitext(os.path.basename(os.path.abspath(altiwx.input_file)))[0]
 
-wavfile = tempdir+output_file + ".wav"
+    #altiwx.debug("dbg point");
 
+    command = "cp '"+altiwx.input_file+"' " + tempdir
 
-outflag = "-N "
-if altiwx.southbound:
-    outflag = "-S "
+    altiwx.info("Processing NOAA APT data...(extended version)")
 
-command = "wxmap -T '"+altiwx.satellite_name+"' -G /home/pi/.wxtoimg '" + \
-    datetime_object.strftime("%H:%M:%S") + "' "+tempdir+output_file+"_map.png"
-
-
-altiwx.debug(command)
-subprocess.Popen([command], shell=1).wait()
-
-mapfile = tempdir+output_file+"_map.png"
-outflag = outflag + "-M '"+mapfile+"'"
-
-
-for enhencement in enh:
-    command = "wxtoimg " + outflag + " -e " + enhencement + outflag + " '" + \
-        altiwx.input_file + "' " + tempdir + output_file + "_"+enhencement+".png"
     altiwx.debug(command)
     subprocess.Popen([command], shell=1).wait()
-altiwx.info("Done processing NOAA APT data!")
+
+
+    wavfile = tempdir+output_file
+
+
+    outflag = "-N "
+    if altiwx.southbound:
+        outflag = "-S "
+
+    command = "wxmap -T '"+altiwx.satellite_name+"' -H 'noaa.txt' '" + \
+        datetime_object.strftime("%d %m %Y %H:%M:%S") + "' "+tempdir+output_file+"_map.png"
+
+
+    altiwx.debug(command)
+    subprocess.Popen([command], shell=1).wait()
+
+    mapfile = tempdir+output_file+"_map.png"
+    outflag = outflag + "-m '"+mapfile+"'"
+
+    outdir = os.path.dirname(os.path.abspath(altiwx.input_file)) + "/"
+
+    command = "wxtoimg " + outflag + " '" + altiwx.input_file + "' '" + outdir + output_file + "_raw.png'"
+    altiwx.debug(command)
+    subprocess.Popen([command], shell=1).wait()
+
+    for enhencement in enh:
+        command = "wxtoimg " + outflag + " -e " + enhencement + " '" + \
+            altiwx.input_file + "' '" + outdir + output_file + "_" + enhencement + ".png'"
+        altiwx.debug(command)
+        subprocess.Popen([command], shell=1).wait()
+    altiwx.info("Done processing NOAA APT data!")
+
+except Exception as e:
+    exception_type, exception_object, exception_traceback = sys.exc_info()
+    filename = exception_traceback.tb_frame.f_code.co_filename
+    line_number = exception_traceback.tb_lineno
+
+    print("Exception type: ", exception_type)
+    print("Object: ", exception_object)
+    print("Traceback: ", exception_traceback)
+    print("File name: ", filename)
+    print("Line number: ", line_number)
